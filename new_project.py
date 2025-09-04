9cff635afae95753db325df5e5d2edd95f3849b7
Stanislav Nuzhdin <ledawe32@gmail.com>
2025-09-04 20:53:56 +0300

    real working

--- new_project.py
+++ new_project.py
@@ -1,1 +1,61 @@
-print("Hello world fdsfsdfdffds")
+import socket
+from tkinter import *
+
+
+#Решаем вопрос с кирилицей
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
+# -----------------------------
+
+tk=Tk()
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+s.bind(('0.0.0.0',11719))
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST,1)
+
+text=StringVar()
+name=StringVar()
+name.set('You')
+text.set('')
+tk.title('MegaChat')
+tk.geometry('400x300')
+
+log = Text(tk)
+nick = Entry(tk, textvariable=name)
+msg = Entry(tk, textvariable=text)
+msg.pack(side='bottom', fill='x', expand='true')
+nick.pack(side='bottom', fill='x', expand='true')
+log.pack(side='top', fill='both',expand='true')
+
+def loopproc():
+	log.see(END)
+	s.setblocking(False)
+	try:
+		message = s.recv(128)
+		message = message.decode('utf-8')  # для Python 3 
+		log.insert(END,message+'\n')
+	except:
+		tk.after(1,loopproc)
+		return
+	tk.after(1,loopproc)
+	return
+
+def sendproc(event):
+    if event.keysym =='Return':
+        message = name.get()+':'+text.get()
+        message = message.encode('utf-8')
+        sock.sendto (message,('255.255.255.255',11719))
+        text.set('')
+        return 'break'
+    
+
+msg.bind('<KeyPress>',sendproc)   
+
+msg.focus_set()
+
+tk.after(1,loopproc)
+tk.mainloop()
